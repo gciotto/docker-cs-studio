fd200fc2895cd1f607771fe22f564a767065cd4c
Gustavo Ciotto <gustavociotto@gmail.com>
2017-05-18 14:53:21 -0300

    script to compile lnls-studio

---
+++ setup-lnls-studio.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd ${LNLSTUDIO_PATH}
+
+MAVEN_FLAGS="-Dcsstudio.composite.repo=${CSSTUDIO_REPO}"
+MAVEN_GOALS="tycho-p2-director:archive-products"
+
+echo "Building branch ${1} ..."
+git checkout ${1}
+
+mvn ${MAVEN_FLAGS} ${MAVEN_GOALS}
+
+mkdir -p ${LNLSTUDIO_REPO}/${2}
+
+cp ${LNLSTUDIO_PATH}/repository/target/products/*.tar.gz ${LNLSTUDIO_REPO}/${2}
+cp ${LNLSTUDIO_PATH}/repository/target/products/*.zip ${LNLSTUDIO_REPO}/${2}
+
+mvn clean
+
